21cea4b5313d92e4817686854977c4a5a0fef538
Pavel Gromov <pagrom27@gmail.com>
2021-04-08 22:39:39 +0300

    Update sql schema

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -1,16 +1,26 @@
-drop table if exists message_template CASCADE;
-drop table if exists message_template_recipients CASCADE;
+drop table if exists template CASCADE;
+drop table if exists template_recipients CASCADE;
+drop table if exists template_validation CASCADE;
 
-create table message_template
+create table template
 (
     template_id varchar(255) not null,
     template    varchar(255) not null,
     primary key (template_id)
 );
-create table message_template_recipients
+create table template_recipients
 (
-    message_template_template_id varchar(255) not null,
-    recipients                   varchar(255) not null
+    template_template_id varchar(255) not null,
+    recipients           varchar(255) not null
 );
-alter table message_template_recipients
-    add constraint FKptr0oxm6di5b1kf9bsmrr49gh foreign key (message_template_template_id) references message_template;
+create table template_validation
+(
+    template_template_id varchar(255) not null,
+    validation           varchar(255),
+    validation_key       varchar(255) not null,
+    primary key (template_template_id, validation_key)
+);
+alter table template_recipients
+    add constraint FKrfb44ummv3hkq5n9h9pgd85ki foreign key (template_template_id) references template;
+alter table template_validation
+    add constraint FKdgpifyff79ju0bv9vx3r9bi1u foreign key (template_template_id) references template;
